825b7fa007c975ea081ff5606f30045b6e7bf7a7
Tom Breuel <tmbdev@gmail.com>
2017-10-17 15:58:50 -0700

    added loadable parallel input modules

--- dlinputs/parallel.py
+++ dlinputs/parallel.py
@@ -1,21 +1,32 @@
 # Copyright (c) 2017 NVIDIA CORPORATION. All rights reserved.
 # See the LICENSE file for licensing terms (BSD-style).
 
+import loadable
+
 try:
     import torch.multiprocessing as mp
 except:
     import multiprocessing as mp
 
 
-def parallel_job(factory, queue, index):
-    data = factory()
+def _parallel_job(factory, args, queue, index):
+    """Helper function to start up an input queue in a subprocess."""
+    data = factory(*args)
     for sample in data:
+        sample["__thread__"] = index
         queue.put(sample)
 
 
-def parallelize_input(factory, nthreads=4, maxsize=256):
+def parallelize_input(factory, args=(), nthreads=4, maxsize=256):
+    """Parallelize an input queue.
+
+    :param factory: factory function for input queue
+    :param args: arguments to factory function
+    :param nthreads: number of subprocesses
+    :param maxsize: maximum queue size for input queue
+    """
     queue = mp.Queue(maxsize)
-    jobs = [mp.Process(target=parallel_job, args=(factory, queue, i))
+    jobs = [mp.Process(target=_parallel_job, args=(factory, args, queue, i))
             for i in range(nthreads)]
     for job in jobs:
         job.start()
@@ -23,3 +34,19 @@ def parallelize_input(factory, nthreads=4, maxsize=256):
         sample = queue.get()
         sample["__queue_size__"] = queue.qsize()
         yield sample
+
+def _factory(fname, method):
+    """Helper function for parallel_load."""
+    inputs = loadable.load_input(fname)
+    f = getattr(inputs, method)
+    return f()
+
+def parallel_load(fname, method="training_data", nthreads=4, maxsize=256):
+    """Load the input pipeline and execute it in parallel.
+
+    :param fname: filename containing the input pipepline (.py)
+    :param method: method on ``Inputs`` object (default: training_data)
+    :param nthreads: number of subprocesses
+    :param maxsize: maximum queue size for input queue
+    """
+    return parallelize_input(_factory, (fname, method), nthreads=nthreads, maxsize=maxsize)
