54418de33b5e501b6eec27cb540110db441321e6
nsonti <nithin.sonti@gmail.com>
2018-05-24 11:21:38 -0700

    [backup gopen] partially working

--- dlinputs/gopen.py
+++ dlinputs/gopen.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 from builtins import range
+from io import open
 import os
 import pdb
 import random
@@ -21,24 +22,26 @@ def test_curl_write(self, location):
         raise Exception("{}: cannot write location".format(location))
     check_call(["curl", "--fail", "-X", "DELETE", location])
 
-def gopen(url, mode="rb"):
+# Python 3 os.popen() only has mode = "r" or "w"
+# Python 2 code was using default mode as "rb"; changing it to only "r"
+def gopen(url, mode="r"):
     """Open the given URL. Supports unusual schemes and uses subprocesses."""
     parsed = urlparse(url)
     if parsed.scheme == "gs":
         if mode[0]=="r":
-            return os.popen("gsutil cat '%s'" % url, "rb")
+            return Popen("gsutil cat '%s'" % url, stdout=PIPE, stderr=PIPE, shell=True)
         elif mode[0]=="w":
-            return os.popen("gsutil cp - '%s'" % url, "wb")
+            return Popen("gsutil cp - '%s'" % url, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
         else:
             raise ValueError("{}: unknown mode".format(mode))
     elif parsed.scheme in "http https ftp".split():
         if mode[0]=="r":
             cmd = "curl --fail -s '%s'" % url
-            return os.popen(cmd, "rb")
+            return Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
         elif mode[0]=="w":
             test_curl_write(url)
             cmd = "curl --fail -s -T - '%s'" % url
-            return os.popen(cmd, "wb")
+            return Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
         else:
             raise ValueError("{}: unknown mode".format(mode))
     elif parsed.scheme in ["", "file"]:
@@ -53,7 +56,8 @@ def test_url(url, size=17):
     """Test whether the given URL is accessible."""
     try:
         with gopen(url) as stream:
-            data = stream.read(size)
+            pdb.set_trace()
+            data = stream.communicate()[0][:size]
         if len(data) == size:
             return True
         return False
@@ -72,6 +76,7 @@ def test_shards(url, size=17, complete=False):
 def find_shards(urls, size=17, complete=False):
     """Given a list of shard URLs, find the first one that exists."""
     for url in urls:
+        pdb.set_trace()
         if test_shards(url, size=size, complete=False):
             return url
 
